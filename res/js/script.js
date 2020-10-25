55073e35c3d2781ef257484ca6c22aafad756ae3
Anželika Bidzilja <bidzilja@ut.ee>
2020-10-25 19:24:50 +0200

    Adding half of task2

--- res/js/script.js
+++ res/js/script.js
@@ -1,19 +1,23 @@
 $(function(){
 
-$( "img.logo" ).on('click', ()=>{
-	$(".dropdown").toggle();
-});
+	$( "img.logo" ).on('click', ()=>{
+		$(".dropdown").toggle();
+	});
 
-/*Fetch user profile information */
+	/*Fetch user profile information */
 
-$.get('https://private-anon-27576e448e-wad20postit.apiary-mock.com/users/1')
-	.done((user) => NewUserInfo(user))
-	.fail((error) => console.log(error));
+	$.get('https://private-anon-27576e448e-wad20postit.apiary-mock.com/users/1')
+		.done((user) => NewUserInfo(user))
+		.fail((error) => console.log(error));
 
 
-$.get('http://private-anon-27576e448e-wad20postit.apiary-mock.com/posts')
-	.done((post) => NewPosts(post))
-	.fail((error) => console.log(error));
+	$.get('http://private-anon-27576e448e-wad20postit.apiary-mock.com/posts')
+		.done((post) => NewPosts(post))
+		.fail((error) => console.log(error));
+
+	$( "button.like-button" ).on('click', ()=>{
+		$(".like-button.liked").toggle();
+	});
 
 })
 
@@ -24,7 +28,7 @@ function NewUserInfo(user){
 	$('.logo').attr('src', user.avatar);
 }
 
-let postInfo = 
+let postInfo =
 '<div class="post">'+
         '<div class="post-author">'+
          '<span class="post-author-info">'+
@@ -34,10 +38,10 @@ let postInfo =
           '<small>month day, year 15:16</small>'+
         '</div>'+
         '<div class="post-image">'+
-          '<img src="res/images/posts/1.jpg" alt="">'+
+          '<img src="", alt="This post have no media">'+
         '</div>'+
         '<div class="post-title">'+
-          '<h3>Title</h3>'+
+          '<h3></h3>'+
         '</div>'+
         '<div class="post-actions">'+
           '<button type="button" name="like" class="like-button">100k</button>'+
@@ -50,11 +54,21 @@ function NewPosts(posts){
 		$('.post-author-info img',postTemplate).attr('src', post.author.avatar);
 		$('.post-author-info small',postTemplate).text(post.author.firstname + " " + post.author.lastname);
 		$('.post-author>small',postTemplate).text(post.createTime);
-		$('.post-title>h3',postTemplate).text(post.text);
+		if(post.text !== null){
+			$('.post-title>h3',postTemplate).text(post.text);
+		} else{
+
+		}
+		if(post.media !== null){
+			if(post.media.type === "image"){
+				$('.post-image img', postTemplate).attr('src', post.media.url);
+
+		  }
+			if(post.media.type === "video"){
+				$('.post-image img', postTemplate).replaceWith('<video src="video.mp4", controls></video>').attr('src', post.media.url);
+			}
+		}
 		$('.like-button',postTemplate).text(post.likes);
-
-		//Add images
-
 		$('section.main-container').append(postTemplate);
 	}
 }
